43311b3350f2ee60ff42cc94a7f1bbb47e467a98
hcrapidinnovation <69822624+hcrapidinnovation@users.noreply.github.com>
2022-04-24 17:44:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello d\n'
+  const msg = 'Hell o d\n'
   res.end(msg);
 });
 
